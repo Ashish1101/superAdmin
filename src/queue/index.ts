6f82f40c4d5a4cb0528810bf980b28aeb3f6d6b5
Ashish Personal <ashishskkumar321@gmail.com>
2022-01-23 23:50:41 +0530

    changed some code

--- src/queue/index.ts
+++ src/queue/index.ts
@@ -1,9 +1,7 @@
 import { Channel , ConsumeMessage} from "amqplib";
 
+//no queue activity for superAdmin
 
 export default (channel : Channel) => {
-    channel.consume('adminQueue' , (msg : ConsumeMessage | null) => {
-        const MSG : any = msg?.content
-        console.log('message recieved from superAdminQueue' , JSON.parse(MSG?.toString()))
-    } , {noAck : true})
+   
 }
